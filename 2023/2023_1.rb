9ee3f4a24361f1124bf34c162c8fed31e1ed6286
Angel Vanegas <avanegas@carecloud.com>
2023-12-05 13:09:31 -0500

    First part done

---
+++ 2023/2023_1.rb
@@ -0,0 +1,11 @@
+def converter line
+    tmp = line.chomp.chars.filter {|x| x.match? /[[:digit:]]/}
+  (tmp.first + tmp.last)
+end
+
+jort = []
+File.foreach("calibrate.txt") do |line|
+  jort.push converter(line)
+end
+
+p jort.map(&:to_i).sum
